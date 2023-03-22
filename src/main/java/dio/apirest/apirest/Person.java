fcc11675488e23140c71ae0a24324f6984778fc3
leofab <leofabr3@gmail.com>
2023-03-22 17:29:14 -0300

    Person class implement

---
+++ src/main/java/dio/apirest/apirest/Person.java
@@ -0,0 +1,22 @@
+package dio.apirest.apirest;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String name;
+}

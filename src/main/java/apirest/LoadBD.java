ea31a0a914404940330bf4c7b8954c60779fdf84
leofab <leofabr3@gmail.com>
2023-03-23 15:12:07 -0300

    LoadBD classe Bean and Autowire required = false implement for applicationRunner

--- src/main/java/apirest/LoadBD.java
+++ src/main/java/apirest/LoadBD.java
@@ -1,4 +1,22 @@
 package apirest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class LoadBD {
+    private static final Logger log = LoggerFactory.getLogger(LoadBD.class);
+    //persistindo dados no banco com jpa
+    @Bean
+    @Autowired(required = false)
+    CommandLineRunner applicationRunner(EmployeeRepository employeeRepository){
+        return args -> {
+            log.info("Isto é um LOG de usuário salvo -> "+employeeRepository.save(new Employee("Marcelo", "Frontdesk", "St101, 29 - Reno - NV")));
+            log.info("Isto é um LOG de usuário salvo -> "+employeeRepository.save(new Employee("Crhis", "Cleaner", "Av10, 10 - Reno - NV")));
+        };
+    }
 }

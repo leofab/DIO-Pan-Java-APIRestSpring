b2ff9d15385db50929baa809a26287f08840afd7
leofab <leofabr3@gmail.com>
2023-03-23 14:42:28 -0300

    EmployeeNotFoundException & Advice classses refact

--- src/main/java/apirest/EmployeeNotFoundException.java
+++ src/main/java/apirest/EmployeeNotFoundException.java
@@ -1,5 +1,8 @@
 package apirest;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class EmployeeNotFoundException extends RuntimeException{
     public EmployeeNotFoundException(Long id){
         super("Could not find the id : " + id);

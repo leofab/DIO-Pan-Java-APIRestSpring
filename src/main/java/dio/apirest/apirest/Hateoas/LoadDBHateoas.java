001235f5e2d808e3b063093d43af00d8f7f7e8d5
leofab <leofabr3@gmail.com>
2023-03-23 18:02:12 -0300

    new db info

--- src/main/java/dio/apirest/apirest/Hateoas/LoadDBHateoas.java
+++ src/main/java/dio/apirest/apirest/Hateoas/LoadDBHateoas.java
@@ -22,12 +22,12 @@ public class LoadDBHateoas {
     @Autowired(required = false)
     CommandLineRunner loaddata(EmployeeRepository employeeRepositoryHateoas, OrderRepositoryHateoas orderRepositoryHateoas) {
         return args -> {
-            log.info("Log of save event: " + employeeRepositoryHateoas.save(new EmployeeHateoas("Maria Silva", "Chef",
-                    "avenida silveira dutra 1002")));
-            log.info("log of save event: " + employeeRepositoryHateoas.save(new EmployeeHateoas("John Dutra", "Mecanico",
-                    "rua joao freire 231")));
-            log.info("Log of save event: " + employeeRepositoryHateoas.save(new EmployeeHateoas("Bilbo Baggins", "thief",
-                    "The shine")));
+            log.info("Log of save event: " + employeeRepositoryHateoas.save(new EmployeeHateoas("Adonis", "ChefofFront",
+                    "5th av, 44, 20 - Reno - NV")));
+            log.info("log of save event: " + employeeRepositoryHateoas.save(new EmployeeHateoas("Humberto", "Cleaner",
+                    "long street st, 5, 10 - Reno - NV")));
+            log.info("Log of save event: " + employeeRepositoryHateoas.save(new EmployeeHateoas("Cecil", "Adm",
+                    "49th street, 10, 1 - Reno - NV")));
             orderRepositoryHateoas.save(new OrderHateoas(Status.COMPLETED, "review"));
             orderRepositoryHateoas.save(new OrderHateoas(Status.IN_PROGRESS, "travel"));
             orderRepositoryHateoas.save(new OrderHateoas(Status.IN_PROGRESS, "sale"));
